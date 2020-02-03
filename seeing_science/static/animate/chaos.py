7ef064dd1b33ebd68e2df9a29a3df520c4721c83
MDAddario <michael.lindner.daddario@gmail.com>
2020-02-03 17:19:02 -0500

    Working on pillar detection

--- seeing_science/static/animate/chaos.py
+++ seeing_science/static/animate/chaos.py
@@ -62,34 +62,42 @@ class Ball:
 	
 	def __init__(self, position, direction, pillar_centers, pillar_radius, \
 				min_x, max_x, min_y, max_y, pbc, graph):
+		
 		self.position = position
 		self.velocity = direction * self.speed
 		self.pillar_centers = pillar_centers
 		self.pillar_radius = pillar_radius
-		self.pbc = pbc
 		self.min_x = min_x
 		self.max_x = max_x
 		self.min_y = min_y
 		self.max_y = max_y
+		self.pbc = pbc
 		self.graph = graph
+		
 		self.width_x = max_x - min_x
 		self.width_y = max_y - min_y
 	
 	def update(self):
+		
+		# Move ball forward
 		self.position += self.velocity
 		
-		# Ignore pillar collisions
-		self.graph.set_offsets(self.position)
+		# Check pillar collisions
+		deltas = pillar_centers - self.position[:, None]
+		squares = np.square(deltas)
+		distances = np.sum(squares, axis=0)
+		
+		indices = np.where(distances < self.pillar_radius)
 		
 		# Check wall collisions
 		if self.pbc:
 			if self.position[0] < self.min_x:
 				self.position[0] += self.width_x
-			if self.position[0] > self.max_x:
+			elif self.position[0] > self.max_x:
 				self.position[0] -= self.width_x
 			if self.position[1] < self.min_y:
 				self.position[1] += self.width_y
-			if self.position[1] > self.max_y:
+			elif self.position[1] > self.max_y:
 				self.position[1] -= self.width_y
 		
 		else:
@@ -97,6 +105,9 @@ class Ball:
 				self.velocity[0] *= -1.0
 			if self.position[1] < self.min_y or self.position[1] > self.max_y:
 				self.velocity[1] *= -1.0
+		
+		# Update plot
+		self.graph.set_offsets(self.position)
 
 '''
 Plot system
