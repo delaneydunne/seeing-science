be5977addae2c68ee1d9849b210eaa9866a53bc9
MDAddario <michael.lindner.daddario@gmail.com>
2020-02-03 20:42:29 -0500

    Changing layout to PEP-8

--- seeing_science/static/animate/chaos.py
+++ seeing_science/static/animate/chaos.py
@@ -19,7 +19,7 @@ num = 100
 circle_angles = np.linspace(0, 2 * np.pi, num=num)
 x_pos = np.cos(circle_angles)
 y_pos = np.sin(circle_angles)
-unit_circle = np.vstack((x_pos[None,:], y_pos[None,:]))
+unit_circle = np.vstack((x_pos[None, :], y_pos[None, :]))
 
 '''
 Create static pillars
@@ -32,19 +32,18 @@ pillar_radius = 0.1
 
 pillar_list = []
 pillar_circle = pillar_radius * unit_circle
-pillar_centers = np.empty((2, num_pillars_x**2))
+pillar_centers = np.empty((2, num_pillars_x ** 2))
 
 for x in range(num_pillars_x):
-	for y in range(num_pillars_x):
-		
-		index = x * num_pillars_x + y
-		x_pos = x * pillar_spacing
-		y_pos = y * pillar_spacing
-		pos = np.array([[x_pos, y_pos]])
-		
-		pillar_centers[:, index] = pos
-		pillar_list.append(pillar_circle + pos.T)
-		
+    for y in range(num_pillars_x):
+        index = x * num_pillars_x + y
+        x_pos = x * pillar_spacing
+        y_pos = y * pillar_spacing
+        pos = np.array([[x_pos, y_pos]])
+
+        pillar_centers[:, index] = pos
+        pillar_list.append(pillar_circle + pos.T)
+
 min_dim = -2 * pillar_radius
 max_dim = num_pillars_x - 1 + 2 * pillar_radius
 
@@ -55,78 +54,78 @@ Create ball object
 num_balls = 5
 init_pos = np.array([0.5, 0.5])
 
+
 class Ball:
-	
-	speed = 5e-2
-	
-	def __init__(self, position, direction, pillar_centers, pillar_radius, \
-				min_x, max_x, min_y, max_y, pbc, graph):
-		
-		self.position = position
-		self.velocity = direction / np.linalg.norm(direction) * self.speed
-		self.pillar_centers = pillar_centers
-		self.pillar_radius = pillar_radius
-		self.min_x = min_x
-		self.max_x = max_x
-		self.min_y = min_y
-		self.max_y = max_y
-		self.pbc = pbc
-		self.graph = graph
-		
-		self.width_x = max_x - min_x
-		self.width_y = max_y - min_y
-	
-	def update(self):
-		
-		# Move ball forward
-		self.position += self.velocity
-		
-		# Check pillar collisions
-		deltas = pillar_centers - self.position[:, None]
-		squares = np.square(deltas)
-		mags = np.sum(squares, axis=0)
-		distances = np.sqrt(mags)
-		this = np.where(distances < self.pillar_radius)
-		
-		# Collision with circle
-		if this[0].shape[0] != 0:
-			
-			center = pillar_centers[:,this[0]][:,0]
-			direction = self.position - center
-			velocity = direction / np.linalg.norm(direction) * self.speed
-			self.velocity = velocity
-		
-		# Check wall collisions
-		if self.pbc:
-			if self.position[0] < self.min_x:
-				self.position[0] += self.width_x
-			elif self.position[0] > self.max_x:
-				self.position[0] -= self.width_x
-			if self.position[1] < self.min_y:
-				self.position[1] += self.width_y
-			elif self.position[1] > self.max_y:
-				self.position[1] -= self.width_y
-		
-		else:
-			if self.position[0] < self.min_x or self.position[0] > self.max_x:
-				self.velocity[0] *= -1.0
-			if self.position[1] < self.min_y or self.position[1] > self.max_y:
-				self.velocity[1] *= -1.0
-		
-		# Update plot
-		self.graph.set_offsets(self.position)
+    speed = 5e-2
+
+    def __init__(self, position, direction, pillar_centers, pillar_radius,
+                 min_x, max_x, min_y, max_y, pbc, graph):
+
+        self.position = position
+        self.velocity = direction / np.linalg.norm(direction) * self.speed
+        self.pillar_centers = pillar_centers
+        self.pillar_radius = pillar_radius
+        self.min_x = min_x
+        self.max_x = max_x
+        self.min_y = min_y
+        self.max_y = max_y
+        self.pbc = pbc
+        self.graph = graph
+
+        self.width_x = max_x - min_x
+        self.width_y = max_y - min_y
+
+    def update(self):
+
+        # Move ball forward
+        self.position += self.velocity
+
+        # Check pillar collisions
+        deltas = pillar_centers - self.position[:, None]
+        squares = np.square(deltas)
+        mags = np.sum(squares, axis=0)
+        distances = np.sqrt(mags)
+        this = np.where(distances < self.pillar_radius)
+
+        # Collision with circle
+        if this[0].shape[0] != 0:
+            center = pillar_centers[:, this[0]][:, 0]
+            direction = self.position - center
+            velocity = direction / np.linalg.norm(direction) * self.speed
+            self.velocity = velocity
+
+        # Check wall collisions
+        if self.pbc:
+            if self.position[0] < self.min_x:
+                self.position[0] += self.width_x
+            elif self.position[0] > self.max_x:
+                self.position[0] -= self.width_x
+            if self.position[1] < self.min_y:
+                self.position[1] += self.width_y
+            elif self.position[1] > self.max_y:
+                self.position[1] -= self.width_y
+
+        else:
+            if self.position[0] < self.min_x or self.position[0] > self.max_x:
+                self.velocity[0] *= -1.0
+            if self.position[1] < self.min_y or self.position[1] > self.max_y:
+                self.velocity[1] *= -1.0
+
+        # Update plot
+        self.graph.set_offsets(self.position)
+
 
 '''
 Plot system
 '''
 
 # Initiate axes
-fig = plt.figure(figsize=(6,6))
+fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
 
 # Initial pillar posititions
 for pillar_line in pillar_list:
-	ax.plot(pillar_line[0,:], pillar_line[1,:], '-r')
+    ax.plot(pillar_line[0, :], pillar_line[1, :], '-r')
 
 # Initial ball positions
 ball_list = []
@@ -135,21 +134,19 @@ dir_list = []
 angles = np.linspace(0.1, 2.7, num=num_balls)
 
 for i in range(num_balls):
-		
-	dir_list.append(np.array([np.cos(angles[i]), np.sin(angles[i])]))
-	graph_list.append(ax.scatter(init_pos[0], init_pos[1], c='g'))
-	
-	ball_list.append(Ball(init_pos, dir_list[i], pillar_centers, \
-				pillar_radius, min_dim, max_dim, min_dim, max_dim, \
-				True, graph_list[i]))
+    dir_list.append(np.array([np.cos(angles[i]), np.sin(angles[i])]))
+    graph_list.append(ax.scatter(init_pos[0], init_pos[1], c='g'))
+
+    ball_list.append(Ball(init_pos, dir_list[i], pillar_centers, \
+                          pillar_radius, min_dim, max_dim, min_dim, max_dim, \
+                          True, graph_list[i]))
+
 
 # Update axes
 def update(frame, ball_list):
-	for ball in ball_list:
-		ball.update()
-		if frame == 2:
-			print(ball.velocity)
-			print(ball.position)
+    for ball in ball_list:
+        ball.update()
+
 
 '''
 Configure plot settings
@@ -165,6 +162,6 @@ ax.set_yticks([])
 ANIMATE THE DIAGRAM
 '''
 
-ani = FuncAnimation(fig, update, tot_frames, fargs=[ball_list], interval=1000/fps)
-#ani.save('chaos.gif', writer='imagemagick', fps=fps)
+ani = FuncAnimation(fig, update, tot_frames, fargs=[ball_list], interval=1000 / fps)
+# ani.save('chaos.gif', writer='imagemagick', fps=fps)
 plt.show()
